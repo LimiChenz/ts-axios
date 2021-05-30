229d1d7048a26b476bbbde52d84ba65679345b04
LimiChenz <1276378329@qq.com>
2021-05-30 22:18:10 +0800

    2021530

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -16,7 +16,42 @@ export function buildURL(url: string, params?: any): string {
         return url
     }
     const parts: string[] = []
+    
+    Object.keys(params).forEach(key=>{
+        let value = params[key]
+        if (value === null || typeof value === 'undefined') {
+            return
+        }
+        let values:string[]
+        if (Array.isArray(value)) {
+            values = value;
+            key += '[]'
+        }else{
+            values = [value]
+        }
 
+        values.forEach(val =>{
+            if (isDate(val)) {
+                val = val.toISOString();
+            }else if(isObject(val)){
+                val = JSON.stringify(val)
+            }
+
+            parts.push(`${encode(key)}=${encode(val)}`)
+        })
+
+    })
+
+    let serializedParams = parts.join('&')
+
+    if (serializedParams) {
+        const markIndex = url.indexOf('#');
+        if (markIndex !== -1) {
+            url = url.slice(0,markIndex)
+        }
+
+        url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams;
+    }
 
     return url
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import { buildURL } from './helpers/url';
 import { AxiosRequestConfig } from './types'
+import xhr from './xhr';
 
 function axios(config: AxiosRequestConfig) {
-    // 
+    processConfig(config);
+    xhr(config);
 }
 
+function processConfig(config: AxiosRequestConfig):void{
+    config.url = transformUrl(config);
+}
 
+function transformUrl(config: AxiosRequestConfig): string{
+    const {url,params} = config;
+    return buildURL(url,params);
+}
 
 export default axios;
