96a8367663df9d6e49b6fe5b4c36147288fcebb9
LimiChenz <1276378329@qq.com>
2021-05-31 22:32:32 +0800

    webpack server

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,18 +1,18 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig) {
-    const { data, params, method = 'get', url, headers } = config;
-    const xhr = new XMLHttpRequest();
+  const { data, params, method = 'get', url, headers } = config
+  const xhr = new XMLHttpRequest()
 
-    xhr.open(method.toUpperCase(), url, true);
+  xhr.open(method.toUpperCase(), url, true)
 
-    Object.keys(headers).forEach(name => {
-        if (data === null && name.toUpperCase() === 'CONTENT-TYPE') {
-            delete headers[name]
-        } else {
-            xhr.setRequestHeader(name, headers[name])
-        }
-    })
+  // Object.keys(headers).forEach(name => {
+  //     if (data === null && name.toUpperCase() === 'CONTENT-TYPE') {
+  //         delete headers[name]
+  //     } else {
+  //         xhr.setRequestHeader(name, headers[name])
+  //     }
+  // })
 
-    xhr.send(data);
+  xhr.send(data)
 }
